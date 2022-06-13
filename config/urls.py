b3f696263897266eb4d71d21f4076dde0a36619b
David Factorial <david@recursivegarden.com>
2022-06-13 12:56:37 -0600

    fix toolbar

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,3 @@
-import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
@@ -13,6 +12,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
