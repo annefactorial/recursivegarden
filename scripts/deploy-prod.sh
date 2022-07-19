ea11e0f0382affd740a4987621aca0e1209c23a2
Anne Factorial <anne@recursivegarden.com>
2022-07-18 17:40:50 -0700

    Remove unworking server update script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Copy server config files and reload/restart server services
-./scripts/deploy-server-config.sh root@labyrinth.love
+#./scripts/deploy-server-config.sh root@labyrinth.love
 
 # Do Django deployment on the remote server
 cat ./scripts/deploy-django.sh | ssh recursivegarden@labyrinth.love /bin/bash
